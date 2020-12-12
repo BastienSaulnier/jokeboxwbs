0aea48333074ca302409e9bb4902be16c581928e
BastienSaulnier <bastien.saul@gmail.com>
2020-12-12 09:24:49 +0100

    added logger, indexRouter, cors to app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import logger from "morgan";
 import cors from "cors";
 
+const indexRouter = require("./routes/index");
 const port = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
+
+app.use(logger("dev"));
+app.use("/", indexRouter);
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://preprod.dashboard.unsold.fr"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Methods",
+      "Access-Control-Request-Headers",
+    ],
+    credentials: true,
+    // enablePreflight: true
+  })
+);
 
 app.listen(port, (err) => {
   if (err) {
