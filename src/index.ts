5441c95a90cc8aebe291dff05347b90f9db91e05
BastienSaulnier <bastien.saul@gmail.com>
2021-01-15 08:23:55 +0100

    destructured .env vars for index

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,50 @@
+import { init, autorisation, token } from "./utils/auth";
 import logger from "morgan";
 import cors from "cors";
 
+const colors = require("colors/safe");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const indexRouter = require("./routes/index");
+const ORIGIN = process.env.DEV_CORS_ORIGIN_URL;
+const PORT = process.env.DEV_BACKEND_PORT;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://personaljokebox.netlify.app"],
+    origin: [ORIGIN],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
       "Access-Control-Allow-Methods",
       "Access-Control-Request-Headers",
     ],
-
     credentials: true,
-    // enablePreflight: true
+    // enablePreflight: true,
   })
 );
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/static", express.static(__dirname + "/../static"));
+
+app.use((req, res, next) => init(req, res, next));
+app.use((req, res, next) => autorisation(req, res, next));
+app.use((req, res, next) => token(req, res, next));
 app.use("/", indexRouter);
 
-app.listen(port, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     return console.error(err);
   }
-  return console.log(`Server is now listening on port: *[${port}]*`);
+  return console.log(
+    "Server is now listening on port: " +
+      colors.green("*[ ") +
+      PORT +
+      colors.green(" ]*")
+  );
 });
